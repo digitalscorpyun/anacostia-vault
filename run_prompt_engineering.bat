82302c398ad65a657414344775f4c4ccc15c12b0
digitalscorpyun <mikerkibbe73@gmail.com>
2025-04-05 20:25:02 -0700

    04052025

--- run_prompt_engineering.bat
+++ run_prompt_engineering.bat
@@ -2,6 +2,6 @@
 :: Activate the environment
 CALL C:\ProgramData\Anaconda3\Scripts\activate lion
 :: Run your Python script
-python "C:\Users\digitalscorpyun\Projects & Coding\scripts\prompt_engineering.py"
+python "C:\Users\digitalscorpyun\projects-and-coding\scripts\prompt_engineering.py"
 :: Keep the terminal open after execution
 pause
